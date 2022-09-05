50e016d5f65df9dea9531eb477d1a3edea345f0a
feras alnowiser <ferasdev@gmail.com>
2022-09-05 22:46:38 +0300

    adding the get and listen function from express library

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -3,3 +3,12 @@ console.log('Week 5 Day 2 - Express JS');
 
 const express = require('express');
 const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send('This is respond GET');
+});
+
+app.listen(port, () => {
+    console.log('SERVER ARE WORKING ...');
+});
