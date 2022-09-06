acfad461b1af46377ada203b283dd78dadcdcc82
feras alnowiser <ferasdev@gmail.com>
2022-09-06 22:10:40 +0300

    adding the data array which will be used to send respond when it called

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -5,6 +5,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const data = [
+    {user:"Feras", password:"1234"},
+    {user:"Ahmad", password:"5678"},
+    {user:"Khalid", password:"1020"},
+    {user:"Nasser", password:"1122"},
+    {user:"Yousif", password:"1111"}
+]
+
 app.get("/", (req, res) => {
     res.send('This is respond GET http://');
 });
@@ -14,7 +22,7 @@ app.post("/", (req, res) => {
 });
 
 app.put("/", (req, res) => {
-    res.send('This is respond PUT http://');
+    res.send('This is respond PUT http://localhost:3000');
 });
 
 app.delete("/", (req, res) => {
